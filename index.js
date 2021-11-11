ca6afc7a94ccf179aa4d946759a6faf9e54446b9
Tiago Cacilhas <tiagocacilhas20@gmail.com>
2021-11-11 21:03:52 -0100

    codigo

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static('./public'))
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(_dirname, '/public/index.html'))
+})
+
+app.get('/:nome', function (req, res) {
+    res.send(`Olá ${req.params.nome}`)
+  })
+   
+  const port = 3000;
+
+app.listen(port, function (){
+  console.log("Listenning on port:" + port )
+})   
